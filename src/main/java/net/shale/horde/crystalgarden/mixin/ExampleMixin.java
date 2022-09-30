dcb4d51e9a9f7a685bb06724d65fd6f1efea2687
Lucy <gakupo.gumi@gmail.com>
2022-09-30 18:28:24 +1300

    Init

---
+++ src/main/java/net/shale/horde/crystalgarden/mixin/ExampleMixin.java
@@ -0,0 +1,16 @@
+package net.shale.horde.crystalgarden.mixin;
+
+import net.shale.horde.crystalgarden.Main;
+import net.minecraft.client.gui.screen.TitleScreen;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
+
+@Mixin(TitleScreen.class)
+public class ExampleMixin {
+	@Inject(at = @At("HEAD"), method = "init()V")
+	private void init(CallbackInfo info) {
+		Main.LOGGER.info("This line is printed by an example mod mixin!");
+	}
+}
